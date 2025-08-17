7cf2ebc6bb145e4c36136e6c7777b0d133e2cb2d
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2025-08-18 00:15:33 +0530

    refactoring the courses application code

---
+++ CoursesApplication/src/main/java/com/rahulshettyacademy/controller/AllCourseDetailsController.java
@@ -0,0 +1,53 @@
+package com.rahulshettyacademy.controller;
+
+import java.util.List;
+
+import com.rahulshettyacademy.entity.AllCourseDataEntity;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.rahulshettyacademy.repository.CoursesRepository;
+
+
+@Tag(
+        name = "Courses API's",
+        description = "We have the APIs for courses"
+)
+@RestController
+public class AllCourseDetailsController {
+
+
+    @Autowired
+    CoursesRepository repository;
+
+
+    @CrossOrigin
+    @GetMapping("/allCourseDetails")
+    public List<AllCourseDataEntity> GetCourses() {
+
+        return repository.findAll();
+
+
+    }
+
+
+    @CrossOrigin
+    @RequestMapping("/getCourseByName/{name}")
+    public AllCourseDataEntity getBookById(@PathVariable(value = "name") String name) {
+        try {
+            AllCourseDataEntity lib = repository.findById(name).get();
+            return lib;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+}
